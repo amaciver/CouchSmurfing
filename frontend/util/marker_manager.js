556d02b7dd8ff6fa916a49335a9af8e1ff241f37
Andrew MacIver <Andrew@Andrews-MacBook-Air-4.local>
2017-03-17 18:18:08 -0700

    write some map

---
+++ frontend/util/marker_manager.js
@@ -0,0 +1,46 @@
+/* global google:false */
+
+export default class MarkerManager {
+  constructor(map, handleClick){
+    this.map = map;
+    this.handleClick = handleClick;
+    this.markers = [];
+    //permanently bind instance methods
+    this._createMarkerFromHost = this._createMarkerFromHost.bind(this);
+    this._removeMarker = this._removeMarker.bind(this);
+    this._markersToRemove = this._markersToRemove.bind(this);
+  }
+
+  updateMarkers(hosts){
+    this.hosts = hosts;
+    this._hostesToAdd().forEach(this._createMarkerFromHost);
+    // this._markersToRemove().forEach(this._removeMarker);
+  }
+
+  _hostsToAdd() {
+    const currentHosts = this.markers.map( marker => marker.hostId );
+    return this.benches.filter( bench => !currentHosts.includes(host.id) );
+  }
+
+  _markersToRemove(){
+    const hostIds = this.hosts.map( host => host.id );
+    return this.markers.filter( marker => !hostIds.includes(marker.hostId) );
+  }
+
+  _createMarkerFromHost(host) {
+    const pos = new google.maps.LatLng(host.lat, host.lng);
+    const marker = new google.maps.Marker({
+      position: pos,
+      map: this.map,
+      hostId: host.id
+    });
+    marker.addListener('click', () => this.handleClick(host));
+    this.markers.push(marker);
+  }
+
+  _removeMarker(marker) {
+    const idx = this.markers.indexOf( marker );
+    this.markers[idx].setMap(null);
+    this.markers.splice(idx, 1);
+  }
+}
