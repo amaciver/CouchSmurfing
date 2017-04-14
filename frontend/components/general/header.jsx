60fbc360a8dcbd78a488c1bc311fbf6d83d211fb
Andrew MacIver <maciver.andrew@gmail.com>
2017-04-14 11:37:16 -0700

    add login buttons to lages when not logged in

--- frontend/components/general/header.jsx
+++ frontend/components/general/header.jsx
@@ -1,12 +1,56 @@
 import React from 'react';
+import Modal from 'react-modal';
 import { withRouter, hashHistory } from 'react-router';
 import SearchContainer from './search_container';
 import UserDropdownContainer from './user_dropdown_container';
+import AuthFormContainer from '../auth/auth_form_container';
 
+const authStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
 
 class Header extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      modalIsOpen: false,
+      formType: "login"
+    };
+    this.openModal = this.openModal.bind(this);
+    this.closeModal = this.closeModal.bind(this);
+    this.closeModalCB = this.closeModalCB.bind(this);
+  }
+
+  componentWillMount() {
+    Modal.setAppElement('body');
+  }
+
+  componentWillUpdate() {
+  }
+
+  openModal(formType) {
+    return () => {
+      this.setState({modalIsOpen: true, formType:formType});
+    }
+  }
+
+  closeModal() {
+    this.setState({modalIsOpen: false}, () => this.props.clearErrors())
+  }
+  closeModalCB(cb) {
+    this.setState({modalIsOpen: false}, ()=> {
+      this.props.clearErrors();
+      if (cb) {
+        cb();
+      }
+    });
   }
 
   render(){
@@ -18,8 +62,39 @@ class Header extends React.Component {
       dropdown = <div>
         <UserDropdownContainer />
       </div>
+    } else {
+      dropdown =
+      <div>
 
+        <div>
+          <ul className='header-nav-buttons-list'>
+            <li className='header-nav-buttons-list-item'>
+              <button className='request-button' onClick={this.openModal('demo')}>Demo</button>
+            </li>
+            <li className='header-nav-buttons-list-item'>
+              <button className='request-button' onClick={this.openModal('signup')}>Sign Up</button>
+            </li>
+            <li className='header-nav-buttons-list-item'>
+              <button className='button mod-outline mod-blue' onClick={this.openModal("login")}>Log In</button>
+            </li>
+          </ul>
+        </div>
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          contentLabel='Auth Modal'
+          style={authStyles}
+          >
+          <AuthFormContainer
+            formType={this.state.formType}
+            closeModal={this.closeModal}
+            closeModalCB={this.closeModalCB}
+            openModal={this.openModal}/>
+
+        </Modal>
+      </div>
     }
+
     return (
       <header className='header'>
         <div id="general-header" className="header-wrapper">
